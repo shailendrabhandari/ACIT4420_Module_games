efda3227176f6f66dff5079e94e19cbbbfefb7b4
shailendrabhandari <shailendra.vandari@gmail.com>
2025-09-24 10:42:26 +0200

    acit4420_25

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ACIT_GAME_Module",  # The package name
-    version="0.1",
-    packages=find_packages(),  # Automatically find all packages in your project
-    include_package_data=True,  # Include non-Python files specified in MANIFEST.in (if any)
+    name="ACIT_GAME_Module",
+    version="0.2",
+    packages=find_packages(),  # Includes the 'games' package
+    py_modules=['main'],       # Includes main.py as a top-level module
+    include_package_data=True,
     description="A fun game module as a part of ACIT4420 the lecture series. Basically from Lecture 5, 6, and 7.",
     author="Shailendra Bhandari",
     author_email="shailendra.bhandari@oslomet.no",
-    install_requires=[
-        # List your project's dependencies here, if any, e.g.,
-        # 'requests',
-    ],
+    install_requires=[],
     entry_points={
         'console_scripts': [
-            'games=games.main:main',  # Points directly to the main function in main.py
+            'games=main:main',  # Points to main() in main.py (root directory)
         ],
     },
 )
-
